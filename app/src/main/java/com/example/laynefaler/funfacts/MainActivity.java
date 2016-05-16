bc4dde0895a7ff815b858f5f4bc51f385bf78566
lfaler <laynefaler@gmail.com>
2016-05-16 15:34:32 -0700

    Finished app

--- app/src/main/java/com/example/laynefaler/funfacts/MainActivity.java
+++ app/src/main/java/com/example/laynefaler/funfacts/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.laynefaler.funfacts;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        private FactBook mFactBook = new FactBook();
+        private ColorWheel mColorWheel = new ColorWheel();
+        // Declare our View variables
+        private TextView mFactTextView;
+        private Button mShowFactButton;
+        private RelativeLayout mRelativeLayout;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // Assign the Views from the layout file to the corresponding variables
+            mFactTextView = (TextView) findViewById(R.id.factTextView);
+            mShowFactButton = (Button) findViewById(R.id.showFactButton);
+            mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+            View.OnClickListener listener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String fact = mFactBook.getFact();
+                    int color = mColorWheel.getColor();
+
+                    //Update the screen with our dynamic fact
+                    mFactTextView.setText(fact);
+                    mRelativeLayout.setBackgroundColor(color);
+                    mShowFactButton.setTextColor(color);
+                }
+            };
+            mShowFactButton.setOnClickListener(listener);
+        }
     }
-}
